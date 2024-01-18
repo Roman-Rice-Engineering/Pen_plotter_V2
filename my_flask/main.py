cb9af6dbae990b4a7c9185e43f16e1ebae896aa1
ShubhRobotic <shubhpatl2006@gmail.com>
2024-01-18 19:00:42 +0000

    fixed vpype config not updating and added pdftocairo installer

--- my_flask/main.py
+++ my_flask/main.py
@@ -5,19 +5,7 @@ import subprocess
 import os
 import glob
 import serial
-import RPi.GPIO as GPIO
-import time
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
 
-motor_PWM = 18
-sensor1 = 14
-motor_Direction = 15
-GPIO.setup(motor_PWM, GPIO.OUT)
-GPIO.setup(sensor1, GPIO.IN)
-GPIO.setup(motor_Direction, GPIO.OUT)
-motor = GPIO.PWM(motor_PWM, 100)
-motor.start(0)
 
 app = Flask(__name__, static_folder="static",)
 app.config["SECRET_KEY"] = "Gooseberry"
@@ -250,23 +238,6 @@ def Print():
     flash("Gcode Uploaded")
     ser.close()
     # motor roller
-    while True:
-        sensor = GPIO.input(sensor1)
-        GPIO.output(motor_Direction, GPIO.LOW)
-        motor.ChangeDutyCycle(100)
-        time.sleep(0.5)
-        if sensor == 1:
-            GPIO.output(motor_Direction, GPIO.LOW)
-            motor.ChangeDutyCycle(100)
-            print(sensor)
-        else:
-            time.sleep(0.7)
-            GPIO.output(motor_Direction, GPIO.LOW)
-            motor.ChangeDutyCycle(0)
-            time.sleep(0.1)
-            print(sensor)
-            flash("motor stoppeds")
-            break
     return redirect("/")
 
 
